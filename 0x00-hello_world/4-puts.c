836100d295fe9f13e2061c6e126f68d5358ba38a
Shimekit Embibel <embibelshimekit@gmail.com>
2022-04-09 21:41:21 +0300

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multiple puzzle");
+return (0)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
